09be436d5a6a1ae20ef17e51c40c80837702fcdc
Amanda Myntti <amanda.myntti@gmail.com>
2024-03-24 20:12:48 +0200

    Cosine similarity format tests

--- zeroshot/test_driver.py
+++ zeroshot/test_driver.py
@@ -1,14 +1,15 @@
-from piifinder import PiiFinder
+from piimasker import PiiMasker
 import transformers
 
-MODEL_NAME = "xlm-roberta-base"
-#MODEL_NAME="TurkuNLP/bert-base-finnish-cased-v1"
+#MODEL_NAME = "xlm-roberta-base"
+MODEL_NAME="TurkuNLP/bert-base-finnish-cased-v1"
 tokenizer = transformers.AutoTokenizer.from_pretrained(MODEL_NAME)
 model = transformers.AutoModelForPreTraining.from_pretrained(MODEL_NAME)
 
-pf = PiiFinder(model, tokenizer, 1e-2, "BPE")
+pf = PiiMasker(model, tokenizer, 2e-2, tokenizer_type="WordPiece")
 
-text = "Moi, olen Amanda, mulle voit laittaa viestiä osoitteeseen example@outlook.com"
+text = "Moi, olen Amanda, mulle voit laittaa viestiä osoitteeseen amanda@outlook.com"
 
-print(pf.print_pii(text, debug=True))
-#print(pf.find_pii(text))
+print(pf.print_pii(text))#, debug=True))
+#output = print(pf.find_pii(text))
+
