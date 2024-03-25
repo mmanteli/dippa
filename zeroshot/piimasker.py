0053678d16265b95df2cbda5d540db40d4aa6f33
Amanda Myntti <amanda.myntti@gmail.com>
2024-03-25 09:40:16 +0200

    Context aware masking works for both tokenizer types

--- zeroshot/piimasker.py
+++ zeroshot/piimasker.py
@@ -9,10 +9,11 @@ import string
 
 class PiiMasker:
 
-    def __init__(self, model, tokenizer, threshold, choose_n=100, choose_k=10, tokenizer_type="BPE"):
+    def __init__(self, model, tokenizer, threshold, use_context=False, choose_n=100, choose_k=10, tokenizer_type="BPE"):
         self.model = model
         self.tokenizer = tokenizer
         self.threshold = threshold
+        self.use_context = bool(use_context)
         self.choose_n = int(choose_n)
         self.choose_k = int(choose_k)
         self.special_tokens = tokenizer.all_special_tokens
@@ -21,13 +22,13 @@ class PiiMasker:
         self.continuation_marker = {"BPE": "▁", "WordPiece": "##"}[tokenizer_type]
 
     def find_pii(self, text, debug = False):
-        masked_indices, tokenized_text, decoded_text = self.mask(text)
+        masked_indices, tokenized_text, decoded_text, context = self.mask(text)
         if debug: print(masked_indices)
         to_be_redacted = []
         to_be_redacted_words = []
         to_redact_with = []
-        for ind in masked_indices:
-            final_score, predictions = self.get_scores(ind, tokenized_text, debug)
+        for ind, cont in zip(masked_indices, context):
+            final_score, predictions = self.get_scores(ind, tokenized_text, cont, debug)
             word = self.tokenizer.decode(tokenized_text["input_ids"][0][ind])
             if final_score < self.threshold:
                 to_be_redacted.append(ind)
@@ -37,10 +38,12 @@ class PiiMasker:
         return {"decoded_text": decoded_text, "tokenizer_output": tokenized_text, "to_redact_indices": to_be_redacted, "to_redact_words": to_be_redacted_words, "predictions": to_redact_with}
 
     def print_pii(self, text, debug=False):
-        masked_indices, tokenized_text, decoded_text = self.mask(text)
+        masked_indices, tokenized_text, decoded_text, context = self.mask(text)
+        if context == []:
+            context = [[]*len(masked_indices)]
         prints = []
-        for ind in masked_indices:
-            final_score, predictions = self.get_scores(ind, tokenized_text, debug)
+        for ind, cont in zip(masked_indices, context):
+            final_score, predictions = self.get_scores(ind, tokenized_text, cont, debug)
             word = self.tokenizer.decode(tokenized_text["input_ids"][0][ind])
             if final_score < self.threshold:
                 prints.append(f'{word} \t >> {final_score} \t >> Redact')#, preds: {predictions[:4]}')
@@ -49,7 +52,7 @@ class PiiMasker:
         for p in prints:
             print(p)
 
-    
+#---------------------------------MASKING---------------------------------------#
     def get_indices_WordPiece(self, t):
         converted = self.tokenizer.convert_ids_to_tokens(t["input_ids"][0])
         indices=[]
@@ -73,19 +76,118 @@ class PiiMasker:
                     reminder = True
                 indices.append([i])
             else:
-                if converted[i] not in self.special_tokens and indices!=[]:   # here we are only skipping the fact that first token is a special token; indices is empty.
+                if converted[i] not in self.special_tokens and indices!=[]:   
+                    # here we are only skipping the fact that first token is a special token; indices is empty.
                     indices[-1].append(i)
         return indices
 
     def mask(self, text):
+        if self.use_context:
+            return self.context_aware_mask(text)
         t = self.tokenizer(text, return_tensors='pt') # prepare normal tokenized input
         if self.tokenizer_type == "BPE":
             indices = self.get_indices_BPE(t)
         elif self.tokenizer_type == "WordPiece":
             indices = self.get_indices_WordPiece(t)
-        return indices, t, self.tokenizer.decode(t.input_ids[0])
+        return indices, t, self.tokenizer.decode(t.input_ids[0]), [[]]*len(indices)   # []s for empty context
+
+
+#----------------------------CONTEXT AWARE MASKING---------------------------------#
+    def find_same_tokens(self, lst, index):
+        target_value = lst[index]
+        return [i for i, value in enumerate(lst) if value == target_value and i != index]
+    
+    def get_context_indices_WordPiece(self, t):
+        converted = self.tokenizer.convert_ids_to_tokens(t["input_ids"][0])
+        indices=[]
+        words = []
+
+        # first, getting the indices as above, but also saving the words in lowercase
+        for i in range(0, len(t["input_ids"][0])):
+            if converted[i][:2] != self.continuation_marker and converted[i] not in self.special_tokens:
+                indices.append([i])
+                words.append(converted[i].lower())
+            else:
+                if converted[i] not in self.special_tokens and indices!=[]:   
+                    # here we are only skipping the fact that first token is a special token; indices is empty.
+                    indices[-1].append(i)
+                    words[-1] += converted[i][2:].lower()
+
+        indices_context=[]
+        assert len(words)==len(indices), "Issues with masking the sentence."
+
+        # then, map same words to same context: eg. "Ville" in indices [2,3] and [13,14]
+        # => context for first is [13,14] and [2,3] for second.
+        for i in range(len(words)):
+            ind_of_words = self.find_same_tokens(words, i)
+            if ind_of_words != []:
+                #print(words[i],":", ind_of_words, np.array(words)[ind_of_words])
+                current = []
+                for j in ind_of_words:
+                    current+= indices[j]
+                indices_context.append(current)
+            else:
+                indices_context.append([])
+        
+        assert len(indices)==len(indices_context), "Issues with context masking, "+str(len(indices))+"!="+str(len(indices_context))+"\nIndices:\t"+str(indices)+"\nContext:\t"+str(indices_context)
+        return indices, indices_context 
+
+    def get_context_indices_BPE(self, t):
+        converted = self.tokenizer.convert_ids_to_tokens(t["input_ids"][0])
+        indices=[]
+        words = []
+
+        reminder = False   # for BPE, to separate punct, we need to know if the last token was punct
+        for i in range(0, len(t["input_ids"][0])):
+            # for BPE, continuation marker is actually a "starting marker"
+            if reminder or (converted[i][0] == self.continuation_marker and converted[i] not in self.special_tokens) or converted[i] in string.punctuation:
+                reminder=False
+                if converted[i] in string.punctuation:
+                    reminder = True
+                indices.append([i])
+                if converted[i][0] == self.continuation_marker:
+                    words.append(converted[i][1:].lower())
+                else:
+                    words.append(converted[i].lower())
+            else:
+                if converted[i] not in self.special_tokens and indices!=[]:   
+                    # here we are only skipping the fact that first token is a special token; indices is empty.
+                    indices[-1].append(i)
+                    words[-1] += converted[i].lower()
+
+        indices_context=[]
+        assert len(words)==len(indices), "Issues with masking the sentence."
+
+        # then, map same words to same context: eg. "Ville" in indices [2,3] and [13,14]
+        # => context for first is [13,14] and [2,3] for second.
+        for i in range(len(words)):
+            ind_of_words = self.find_same_tokens(words, i)
+            if ind_of_words != []:
+                #print(words[i],":", ind_of_words, np.array(words)[ind_of_words])
+                current = []
+                for j in ind_of_words:
+                    current+= indices[j]
+                indices_context.append(current)
+            else:
+                indices_context.append([])
+        
+        assert len(indices)==len(indices_context), "Issues with context masking, "+str(len(indices))+"!="+str(len(indices_context))+"\nIndices:\t"+str(indices)+"\nContext:\t"+str(indices_context)
+        return indices, indices_context 
 
     
+    def context_aware_mask(self, text):
+        
+        t = self.tokenizer(text, return_tensors='pt') # prepare normal tokenized input
+        if self.tokenizer_type == "BPE":
+            indices, context = self.get_context_indices_BPE(t)
+        elif self.tokenizer_type == "WordPiece":
+            indices,  context = self.get_context_indices_WordPiece(t)
+
+        return indices, t, self.tokenizer.decode(t.input_ids[0]), context
+
+
+#-------------------------------predictions-------------------------------------#
+
     def predict(self, masked, i, true_token, print_results=False):
 
         def to_probability(A):
@@ -119,12 +221,12 @@ class PiiMasker:
     
         
             print("Guesses:",self.tokenizer.decode(top_tokens[0,i,:]))
-            print("Logits: ",top_logits[0,i,:])
-            print("Probs:  ",top_probs[:self.choose_n])
+            #print("Logits: ",top_logits[0,i,:])
+            #print("Probs:  ",top_probs[:self.choose_n])
             print("")
         return word_probability, top_guesses
 
-    def get_scores(self, to_be_masked, tokens, debug):
+    def get_scores(self, to_be_masked, tokens, context, debug):
         """
         Calculates the (aggregated) probability of the given word based on the model prediction.
         For multi-subtoken words, aggregation strategy is gradual unmasking and multiplication.
@@ -147,6 +249,9 @@ class PiiMasker:
             current = to_be_masked[i:]   # this is the token we are CURRENTLY interested in
             for j in current:
                 t["input_ids"][0][j] = self.tokenizer.mask_token_id     # we mask the SUBtokens that are in current
+            if context != []:   # if we have context, mask that
+                for j in context:
+                    t["input_ids"][0][j] = self.tokenizer.mask_token_id 
             if debug:
                 print(self.tokenizer.decode(t["input_ids"][0]))
             # multiply the final score with the predicted probability => aggregates over to_be_masked==one word
--- zeroshot/test_driver.py
+++ zeroshot/test_driver.py
@@ -1,12 +1,15 @@
 from piimasker import PiiMasker
 import transformers
+import sys
 
+context = sys.argv[1]
+use_context = {"False":False, "True":True}[context]
 #MODEL_NAME = "xlm-roberta-base"
 MODEL_NAME="TurkuNLP/bert-base-finnish-cased-v1"
 tokenizer = transformers.AutoTokenizer.from_pretrained(MODEL_NAME)
 model = transformers.AutoModelForPreTraining.from_pretrained(MODEL_NAME)
 
-pf = PiiMasker(model, tokenizer, 2e-2, tokenizer_type="WordPiece")
+pf = PiiMasker(model, tokenizer, 1e-4, use_context=use_context, tokenizer_type="WordPiece")
 
 text = "Moi, olen Amanda, mulle voit laittaa viestiä osoitteeseen amanda@outlook.com"
 
