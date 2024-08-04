12d5c988a135c17a8946ab3225251f7e19739066
Amanda Myntti <amanda.myntti@gmail.com>
2024-08-04 16:36:14 +0300

    Lemmatization succesfully added

--- zeroshot/test_driver.py
+++ zeroshot/test_driver.py
@@ -1,7 +1,8 @@
-from piimasker import PiiMasker
+from piidetector import PiiDetector
 import transformers
 import sys
 import json
+import spacy
 
 context = sys.argv[1]
 use_context = {"False":False, "True":True}[context]
@@ -9,11 +10,13 @@ use_context = {"False":False, "True":True}[context]
 MODEL_NAME="TurkuNLP/bert-base-finnish-cased-v1"
 tokenizer = transformers.AutoTokenizer.from_pretrained(MODEL_NAME)
 model = transformers.AutoModelForPreTraining.from_pretrained(MODEL_NAME)
+lemmatizer = spacy.load("fi_core_news_lg")
 
-pf = PiiMasker(model, tokenizer, 1e-4, use_context=use_context, tokenizer_type="WordPiece")
+pd = PiiDetector(model, tokenizer, lemmatizer, 1e-3, use_context=use_context)#, tokenizer_type="BPE")
 
-text = "Tässä on Amanda. Amandalle voit laittaa viestiä osoitteeseen amanda@outlook.com"
+text = "Tässä on Amanda Myntti. Amandalle voit laittaa viestiä osoitteeseen amanda.myntti@gmail.com"
 
-print(json.dumps(pf.find_pii(text, debug=True)))
-#output = print(pf.find_pii(text))
+print(json.dumps(pd.print_pii(text, debug=True)))
+output = print(pd.find_pii(text))
+print(json.dumps(output))
 
