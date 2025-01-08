4ad09c7c56e5da904c4f219124c5140db28befe8
Amanda Myntti <amanda.myntti@gmail.com>
2025-01-08 17:35:21 +0200

    Test with annotation translation

--- deepl/test.py
+++ deepl/test.py
@@ -4,6 +4,7 @@ import os
 # Saved in environment variable $DEEPL_AUTH_KEY
 print(os.getenv("DEEPL_AUTH_KEY"))
 translator = deepl.Translator(os.getenv("DEEPL_AUTH_KEY"))
+lang="FI"
 """
 result = translator.translate_text(["お元気ですか？", "¿Cómo estás?"], target_lang="EN-GB")
 print(result[0].text)  # "How are you?"
@@ -13,7 +14,7 @@ print(result[1].detected_source_lang)  # "ES"
 """
 
 translator.translate_document_from_filepath(
-    "/scratch/project_2009498/SPACCC_MEDDOCAN/corpus/test/brat/S0004-06142006000500002-2.txt",
-    "translated_fi.txt",
-    target_lang="FI"
+    "/scratch/project_2009498/dippa/deepl/with_annotations.txt",
+    f"translated_with_annotations{lang}.txt",
+    target_lang=lang
 )
